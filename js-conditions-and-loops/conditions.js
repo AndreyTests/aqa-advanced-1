c301823d21913890882c106a95fa2fd465b16343
Andrey <andriimatrix@gmail.com>
2023-09-06 23:33:25 +0300

    Final version of home work

--- js-conditions-and-loops/conditions.js
+++ js-conditions-and-loops/conditions.js
@@ -1,38 +1,38 @@
 //Using "if else"
 const averageGrade = 85;
 
-if (averageGrade <= 59){
+if (averageGrade < 60){
     console.log("Незадовільно");
 
-}else if (averageGrade>60&&averageGrade<=70){
+}else if (averageGrade >= 60 && averageGrade <= 70){
     console.log ("Задовільно")
 
-}else if (averageGrade>71&&averageGrade<=80){
+}else if (averageGrade > 71 && averageGrade <= 80){
     console.log("Добре")
 
-}else if (averageGrade>81&&averageGrade<=90){
+}else if (averageGrade > 81 && averageGrade <=9 0){
     console.log("Дуже добре")
 
-}else if (averageGrade>91&&averageGrade<=100){
+}else if (averageGrade> 91 && averageGrade <= 100){
     console.log("Відмінно")}
 
 // Using "switch case"
 const averageGrade1 = 95;
 
 switch (true) {
-    case averageGrade1 <= 59:
+    case averageGrade1 < 60:
         console.log("Незадовільно")
         break
-    case averageGrade1>60&&averageGrade1<=70:
+    case averageGrade1 >= 60 && averageGrade1 <= 70:
         console.log ("Задовільно")
         break
-    case averageGrade1>71&&averageGrade1<=80:
+    case averageGrade1 > 71 && averageGrade1 <= 80:
         console.log("Добре")
         break
-    case averageGrade1>81&&averageGrade1<=90:
+    case averageGrade1 > 81 && averageGrade1 < =90:
         console.log("Дуже добре")
         break
-    case averageGrade1>91&&averageGrade1<=100:
+    case averageGrade1 > 91 && averageGrade1 <= 100:
         console.log("Відмінно")
         break
     default:
--- js-conditions-and-loops/loops.js
+++ js-conditions-and-loops/loops.js
@@ -3,7 +3,7 @@
 const number = 3;
 
 for (let i = 1; i <= 10; i++) {
-    console.log(`${number} x ${i} = ${number*i}`);
+    console.log(`${number} x ${i} = ${number * i}`);
 }
 
 
