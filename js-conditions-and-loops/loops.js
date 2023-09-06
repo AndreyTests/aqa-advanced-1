b499555c39e1f6c5e5e6309db481c557629c9340
Andrey <andriimatrix@gmail.com>
2023-09-06 23:00:23 +0300

    Added solutions to the files

---
+++ js-conditions-and-loops/loops.js
@@ -0,0 +1,18 @@
+// Using "for"
+
+const number = 3;
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`${number} x ${i} = ${number*i}`);
+}
+
+
+// Using "while"
+
+let i = 1;
+
+while (i <= 10 ) {
+    let multiplication = number*i;
+    console.log (`${number} x ${i} = ${multiplication}`);
+    i++;
+}
