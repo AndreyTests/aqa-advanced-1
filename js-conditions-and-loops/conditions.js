04b09656583702f652b1e539dff976028b6c624e
Andrey <andriimatrix@gmail.com>
2023-09-07 11:14:07 +0300

    Removing mistakes in home work 3

--- js-conditions-and-loops/conditions.js
+++ js-conditions-and-loops/conditions.js
@@ -7,13 +7,13 @@ if (averageGrade < 60){
 }else if (averageGrade >= 60 && averageGrade <= 70){
     console.log ("Задовільно")
 
-}else if (averageGrade > 71 && averageGrade <= 80){
+}else if (averageGrade >= 71 && averageGrade <= 80){
     console.log("Добре")
 
-}else if (averageGrade > 81 && averageGrade <=9 0){
+}else if (averageGrade >= 81 && averageGrade <= 90){
     console.log("Дуже добре")
 
-}else if (averageGrade> 91 && averageGrade <= 100){
+}else if (averageGrade>= 91 && averageGrade <= 100){
     console.log("Відмінно")}
 
 // Using "switch case"
@@ -26,10 +26,10 @@ switch (true) {
     case averageGrade1 >= 60 && averageGrade1 <= 70:
         console.log ("Задовільно")
         break
-    case averageGrade1 > 71 && averageGrade1 <= 80:
+    case averageGrade1 >= 71 && averageGrade1 <= 80:
         console.log("Добре")
         break
-    case averageGrade1 > 81 && averageGrade1 < =90:
+    case averageGrade1 >= 81 && averageGrade1 <= 90:
         console.log("Дуже добре")
         break
     case averageGrade1 > 91 && averageGrade1 <= 100:
