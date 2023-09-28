7f9b5733228b841ca1481c58e2c0930eafe2e0b9
Andriy <andriimatrix@gmail.com>
2023-09-28 17:53:14 +0300

    removedObjects

--- objects/task1.js
+++
@@ -1,7 +0,0 @@
-const student = {};
-student.firstName = "Chuck";
-student.lastName = "Norris";
-student.age = 83;
-student.courses = ["Математика", "Історія", "Програмування","Кунг-Фу"];
-
-console.log (student);
--- objects/task3.js
+++
@@ -1,9 +0,0 @@
-const car = {
-    brand : "Maserati",
-    model : "Quattroporte",
-    year : 2020
-}
-car.year = 2022;
-
-console.log(car);
-
